df3535c97694fd45f65c0b384b27e4858ce19164
unknown <samuel_newhouse@yahoo.com>
2019-10-27 12:11:12 -0500

    Added day 007 code

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -3,20 +3,22 @@
 #include <windows.h>
 #include <stdint.h>
 #include <xinput.h>
+#include <dsound.h>
 
 #define internal static
 #define local_persist static
 #define global_variable static
 
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-
 typedef int8_t int8;
 typedef int16_t int16;
 typedef int32_t int32;
 typedef int64_t int64;
+typedef int32 bool32;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
 struct win32_offscreen_buffer
 {
@@ -40,24 +42,26 @@ struct win32_window_dimension
 // Declare macro for creating function prototype.
 #define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE* pState)
 #define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION* pVibration)
+#define DIRECT_SOUND_CREATE(name) HRESULT WINAPI name(LPGUID pcGuidDevice, LPDIRECTSOUND* ppDS, LPUNKNOWN pUnkOuter)
 
 // Define a type for the prototypes so they can be used as pointers.
 typedef X_INPUT_GET_STATE(x_input_get_state);
 typedef X_INPUT_SET_STATE(x_input_set_state);
+typedef DIRECT_SOUND_CREATE(direct_sound_create);
 
 // Define function stubs to be used by default.
 X_INPUT_GET_STATE(XInputGetStateStub)
 {
-    return 0;
+    return ERROR_DEVICE_NOT_CONNECTED;
 }
 X_INPUT_SET_STATE(XInputSetStateStub)
 {
-    return 0;
+    return ERROR_DEVICE_NOT_CONNECTED;
 }
 
 // Create global function pointers to the stub functions
 global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub; // & operator implied for functions.
-global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub; // & operator implied for functions.
+global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub; // & operator implied for functions...
 
 // Redefine function to use the function pointers.
 #define XInputGetState XInputGetState_
@@ -65,11 +69,25 @@ global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub; // & op
 
 internal void Win32LoadXInput(void)
 {
-    HMODULE XInputLibrary = LoadLibraryA("xinput1_3.dll");
+    // TODO: Test this on Windows 8.
+    HMODULE XInputLibrary = LoadLibraryA("xinput1_4.dll");
+    if(!XInputLibrary)
+    {
+        // TODO: Diagnostic
+        HMODULE XInputLibrary = LoadLibraryA("xinput1_3.dll");
+    }
+
     if(XInputLibrary)
     {
         XInputGetState = (x_input_get_state *)GetProcAddress(XInputLibrary, "XInputGetState");
         XInputSetState = (x_input_set_state *)GetProcAddress(XInputLibrary, "XInputSetState");
+
+        if(!XInputGetState) { XInputGetState = XInputGetStateStub; /* TODO: Diagnostic */}
+        if(!XInputSetState) { XInputSetState = XInputSetStateStub; /* TODO: Diagnostic */}
+    }
+    else
+    {
+        // TODO: Diagnostic
     }
 }
 /* ------------------------------------------------------------------------------------------------------------------*/
@@ -77,6 +95,88 @@ internal void Win32LoadXInput(void)
 // Todo: This is a global for now.
 global_variable bool GlobalRunning;
 global_variable win32_offscreen_buffer GlobalBackBuffer;
+
+internal void Win32InitDSound(HWND Window, int32 SamplesPerSecond, int32 BufferSize)
+{
+    // NOTE: Load the library.
+    HMODULE DSoundLibrary = LoadLibraryA("dsound.dll");
+    if(DSoundLibrary)
+    {
+        direct_sound_create *DirectSoundCreate = (direct_sound_create *)GetProcAddress(DSoundLibrary, "DirectSoundCreate");
+
+        // NOTE: Get a DirectSound object.
+        // NOTE: Double-check this works on XP.
+        IDirectSound *DirectSound;
+        if(DirectSoundCreate && DS_OK == DirectSoundCreate(0, &DirectSound, 0))
+        {
+            WAVEFORMATEX WaveFormat = {};
+            WaveFormat.wFormatTag = WAVE_FORMAT_PCM;
+            WaveFormat.nChannels = 2;
+            WaveFormat.nSamplesPerSec = SamplesPerSecond;
+            WaveFormat.wBitsPerSample = 16;
+            WaveFormat.nBlockAlign = WaveFormat.nChannels * WaveFormat.wBitsPerSample / 8;
+            WaveFormat.nAvgBytesPerSec = WaveFormat.nBlockAlign * WaveFormat.nSamplesPerSec;
+            WaveFormat.cbSize = 0;
+
+            if(DirectSound->SetCooperativeLevel(Window, DSSCL_PRIORITY) == DS_OK)
+            {
+                // TODO: DSBCAPS_GLOBALFOCUS?
+                DSBUFFERDESC BufferDescription = {};
+                BufferDescription.dwSize = sizeof(BufferDescription);
+                BufferDescription.dwFlags = DSBCAPS_PRIMARYBUFFER;
+
+                // NOTE: Create a primary buffer.
+                LPDIRECTSOUNDBUFFER PrimaryBuffer;
+                HRESULT CSBResult = DirectSound->CreateSoundBuffer(&BufferDescription, &PrimaryBuffer, 0);
+                if(CSBResult == DS_OK || CSBResult == DS_NO_VIRTUALIZATION)
+                {
+                    // TODO: DS_NO_VIRTUALIZATION Diagnostic and log.
+                    if(PrimaryBuffer->SetFormat(&WaveFormat) == DS_OK)
+                    {
+                        // NOTE: We have finally set the format.
+                        OutputDebugStringA("Primary buffer format was set.\n");
+                    }
+                    else
+                    {
+                        // TODO: Diagnostic
+                    }
+                }
+                else
+                {
+                    // TODO: Diagnostic
+                }
+            }
+            else
+            {
+                // TODO: Diagnostic
+            }
+
+            // NOTE: Might or might not want DSBCAPS_GETCURRENTPOSITION2 flag.
+            DSBUFFERDESC BufferDescription = {};
+            BufferDescription.dwSize = sizeof(BufferDescription);
+            BufferDescription.dwFlags = 0;
+            BufferDescription.dwBufferBytes = BufferSize;
+            BufferDescription.lpwfxFormat = &WaveFormat;
+
+            LPDIRECTSOUNDBUFFER SecondaryBuffer;
+            // NOTE: Create a secondary buffer.
+            HRESULT CSBResult = DirectSound->CreateSoundBuffer(&BufferDescription, &SecondaryBuffer, 0);
+            if(CSBResult == DS_OK || CSBResult == DS_NO_VIRTUALIZATION)
+            {
+                // TODO: DS_NO_VIRTUALIZATION Diagnostic and log.
+                OutputDebugStringA("Secondary buffer was created.\n");
+            }
+            else
+            {
+                // TODO: Diagnostic
+            }
+        }
+        else
+        {
+            // TODO: Diagnostic
+        }
+    }
+}
 
 internal win32_window_dimension Win32GetWindowDimension(HWND Window)
 {
@@ -193,6 +293,7 @@ internal LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPA
         case WM_KEYUP:
         {
             uint32 VKCode = WParam;
+            bool32 AltKeyIsDown = LParam & (1 << 29);
             bool WasDown = ((LParam & (1 << 30)) != 0);
             bool IsDown = ((LParam & (1 << 31)) == 0);
 
@@ -241,6 +342,12 @@ internal LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPA
                 else if(VKCode == VK_SPACE)
                 {
                 }
+            }
+
+
+            if((VKCode == VK_F4) && AltKeyIsDown)
+            {
+                GlobalRunning = false;
             }
         } break;
 
@@ -305,6 +412,8 @@ int CALLBACK WinMain (HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandL
             GlobalRunning = true;
             int XOffset = 0;
             int YOffset = 0;
+
+            Win32InitDSound(Window, 48000, 48000 * sizeof(int16) * 2);
 
             while(GlobalRunning)
             {
